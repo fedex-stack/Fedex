19da8d68613bc8435c418042014b84ef3ba51dd6
fedex-stack <paullacamerau.sarmy02@gmail.com>
2026-06-19 14:36:01 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,15 @@
+export const STATUS = {
+    PENDING: "Pending",
+    PICKED_UP: "Picked Up",
+    PROCESSING: "Processing",
+    IN_TRANSIT: "In Transit",
+    OUT_FOR_DELIVERY: "Out For Delivery",
+    DELIVERED: "Delivered",
+    DELAYED: "Delayed",
+    HELD: "Held",
+    CANCELLED: "Cancelled"
+};
+
+export const ADMIN_USERNAME = "FedexAdmin";
+
+export const ADMIN_EMAIL = "fedexadmin@fedextracker.com";
